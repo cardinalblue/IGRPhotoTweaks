f522460fc0dc8d3bfb8afb28918ec72394b04465
Raymond Wu <raymond.wu@cardinalblue.com>
2019-01-28 18:22:38 +0800

    Feature/pick analytics (#5)
    * Added analytics extension and event protocol
    
    * Removed EventLogger from IGR
    
    * Updated pinchImage analytics

---
+++ IGRPhotoTweaks/PhotoTweakView/IGRPhotoTweakView+Analytics.swift
@@ -0,0 +1,32 @@
+//
+//  IGRPhotoTweakView+Analytics.swift
+//  HorizontalDial
+//
+//  Created by RaymondWu on 2019/1/25.
+//
+
+import Foundation
+import UIKit
+
+extension IGRPhotoTweakView {
+    public func isFliped() -> Bool {
+        // TODO: isFliped
+        return false
+    }
+
+    public func isRotated() -> Bool {
+        return atan2(self.scrollView.transform.b, self.scrollView.transform.a) != 0
+    }
+
+    public func isStraighten() -> Bool {
+        return self.radians != 0
+    }
+
+    public func isDragCropArea() -> Bool {
+        return self.photoContentView.bounds.size != self.cropView.frame.size
+    }
+
+    public func isPinchImage() -> Bool {
+        return self.didPinchImage
+    }
+}
