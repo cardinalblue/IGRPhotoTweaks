db426161d2917c69012f93f20b911f2ea23198b5
Simon <dev.simonlee@gmail.com>
2019-02-15 18:24:41 +0800

    Disable content inset adjustment

--- IGRPhotoTweaks/PhotoTweakView/IGRPhotoTweakView.swift
+++ IGRPhotoTweaks/PhotoTweakView/IGRPhotoTweakView.swift
@@ -82,6 +82,9 @@ public class IGRPhotoTweakView: UIView {
         
         let scrollView = IGRPhotoScrollView(frame: maxBounds)
         scrollView.center = CGPoint(x: self.frame.width.half, y: self.centerY)
+        if #available(iOS 11, *) {
+            scrollView.contentInsetAdjustmentBehavior = UIScrollView.ContentInsetAdjustmentBehavior.never;
+        }
         scrollView.delegate = self
         scrollView.autoresizingMask = [.flexibleWidth, .flexibleHeight]
         self.addSubview(scrollView)
