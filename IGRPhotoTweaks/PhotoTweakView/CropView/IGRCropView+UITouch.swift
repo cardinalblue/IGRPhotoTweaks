0786c2ade62827ae8a4316d8d81a98edc4018dcc
Raymond Wu <djken0106@gmail.com>
2020-04-22 15:16:17 +0800

    Updated CropView gesture rules

--- IGRPhotoTweaks/PhotoTweakView/CropView/IGRCropView+UITouch.swift
+++ IGRPhotoTweaks/PhotoTweakView/CropView/IGRCropView+UITouch.swift
@@ -12,83 +12,105 @@ extension IGRCropView {
     override public func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
         if touches.count == 1 {
             self.updateCropLines(animate: false)
+
+            // Setup current control state and target frame
+            let location: CGPoint = (touches.first?.location(in: self))!
+            self.currentControlState = ControlState.locate(with: location, on: self.frame)
+            self.currentTargetFrame = self.frame
         }
-        
+
         self.delegate?.cropViewDidStartCrop(self)
     }
-    
+
     override public func touchesMoved(_ touches: Set<UITouch>, with event: UIEvent?) {
+        guard let currentControlState = currentControlState else { return }
+
         if touches.count == 1 {
             let location: CGPoint = (touches.first?.location(in: self))!
-            var frame: CGRect = self.frame
-            
-            let p0 = CGPoint(x: CGFloat.zero, y: CGFloat.zero)
-            let p1 = CGPoint(x: self.frame.size.width, y: CGFloat.zero)
-            let p2 = CGPoint(x: CGFloat.zero, y: self.frame.size.height)
-            let p3 = CGPoint(x: self.frame.size.width, y: self.frame.size.height)
-            
-            if location.distanceTo(point: p0) < kCropViewHotArea {
-                frame.origin.x += location.x
-                frame.size.width -= location.x
-                frame.origin.y += location.y
-                frame.size.height -= location.y
+
+            var frame = self.frame
+            let aspectRatio = aspectRatioHeight / aspectRatioWidth
+            let maximumX: CGFloat = currentTargetFrame.size.width * 0.8
+            let maximumY: CGFloat = currentTargetFrame.size.height * 0.8
+            let minimumFrameSize: CGSize = CGSize(width: currentTargetFrame.size.width * 0.2,
+                                                  height: currentTargetFrame.size.height * 0.2)
+
+            switch currentControlState {
+            case .topLeft:
+                frame.origin.x = min(frame.origin.x + location.x, currentTargetFrame.origin.x + maximumX)
+                frame.size.width = max(frame.size.width - location.x, minimumFrameSize.width)
+                frame.origin.y = min(frame.origin.y + location.y, currentTargetFrame.origin.y + maximumY)
+                frame.size.height = max(frame.size.height - location.y, minimumFrameSize.height)
+            case .topRight:
+                frame.size.width = max(location.x, minimumFrameSize.width)
+                frame.origin.y = min(frame.origin.y + location.y, currentTargetFrame.origin.y + maximumY)
+                frame.size.height = max(frame.size.height - location.y, minimumFrameSize.height)
+            case .bottomLeft:
+                frame.origin.x = min(frame.origin.x + location.x, currentTargetFrame.origin.x + maximumX)
+                frame.size.width = max(frame.size.width - location.x, minimumFrameSize.width)
+                frame.size.height = max(location.y, minimumFrameSize.height)
+            case .bottomRight:
+                frame.size.width = max(location.x, minimumFrameSize.width)
+                frame.size.height = max(location.y, minimumFrameSize.height)
+            case .top:
+                frame.origin.y = min(frame.origin.y + location.y, currentTargetFrame.origin.y + maximumY)
+                frame.size.height = max(frame.size.height - location.y, minimumFrameSize.height)
+            case .bottom:
+                frame.size.height = max(location.y, minimumFrameSize.height)
+            case .left:
+                frame.origin.x = min(frame.origin.x + location.x, currentTargetFrame.origin.x + maximumX)
+                frame.size.width = max(frame.size.width - location.x, minimumFrameSize.width)
+            case .right:
+                frame.size.width = max(location.x, minimumFrameSize.width)
             }
-            else if location.distanceTo(point: p1) < kCropViewHotArea {
-                frame.size.width = location.x
-                frame.origin.y += location.y
-                frame.size.height -= location.y
-            }
-            else if location.distanceTo(point: p2) < kCropViewHotArea {
-                frame.origin.x += location.x
-                frame.size.width -= location.x
-                frame.size.height = location.y
-            }
-            else if location.distanceTo(point: p3) < kCropViewHotArea {
-                frame.size.width = location.x
-                frame.size.height = location.y
-            }
-            else if abs(location.x - p0.x) < kCropViewHotArea {
-                frame.origin.x += location.x
-                frame.size.width -= location.x
-            }
-            else if abs(location.x - p1.x) < kCropViewHotArea {
-                frame.size.width = location.x
-            }
-            else if abs(location.y - p0.y) < kCropViewHotArea {
-                frame.origin.y += location.y
-                frame.size.height -= location.y
-            }
-            else if abs(location.y - p2.y) < kCropViewHotArea {
-                frame.size.height = location.y
-            }
-            
+
             // If Aspect ratio is Freezed reset frame as per the aspect ratio
             if self.isAspectRatioLocked {
-                let newHeight = (self.aspectRatioHeight / self.aspectRatioWidth) * frame.size.width
-                frame = CGRect(x: frame.origin.x,
-                               y: frame.origin.y,
-                               width: frame.size.width,
-                               height: newHeight)
+
+                let size: CGSize = {
+                    switch currentControlState {
+                    case .top, .bottom:
+                        let newWidth = frame.size.height / aspectRatio
+                        return CGSize(width: newWidth, height: frame.size.height)
+                    default:
+                        let newHeight = aspectRatio * frame.size.width
+                        return CGSize(width: frame.size.width, height: newHeight)
+                    }
+                }()
+
+                let offsetY: CGFloat = {
+                    switch currentControlState {
+                    case .topLeft, .topRight, .top, .left, .right:
+                        return frame.size.height - size.height
+                    default:
+                        return 0
+                    }
+                }()
+                let origin = CGPoint(x: frame.origin.x, y: frame.origin.y + offsetY)
+
+                frame = CGRect(origin: origin, size: size)
             }
-            
+
             //TODO: Added test cropViewInsideValidFrame
-            
+
             if (frame.size.width > self.cornerBorderLength
                 && frame.size.height > self.cornerBorderLength) {
                 self.frame = frame
                 // update crop lines
                 self.updateCropLines(animate: false)
-                
+
                 self.delegate?.cropViewDidMove(self)
             }
         }
     }
-    
+
     override public func touchesEnded(_ touches: Set<UITouch>, with event: UIEvent?) {
+        self.currentControlState = nil
         self.delegate?.cropViewDidStopCrop(self)
     }
-    
+
     override public func touchesCancelled(_ touches: Set<UITouch>, with event: UIEvent?) {
-        self.delegate?.cropViewDidStopCrop(self)
+        self.currentControlState = nil
+        self.delegate?.cropViewDidStopCrop(self	)
     }
 }
--- IGRPhotoTweaks/PhotoTweakView/CropView/IGRCropView.swift
+++ IGRPhotoTweaks/PhotoTweakView/CropView/IGRCropView.swift
@@ -13,17 +13,17 @@ public protocol IGRCropViewDelegate : class {
      Calls ones, when user start interaction with view
      */
     func cropViewDidStartCrop(_ cropView: IGRCropView)
-    
+
     /*
      Calls always, when user move touch around view
      */
     func cropViewDidMove(_ cropView: IGRCropView)
-    
+
     /*
      Calls ones, when user stop interaction with view
      */
     func cropViewDidStopCrop(_ cropView: IGRCropView)
-    
+
     /*
      Calls ones, when change a Crop frame
      */
@@ -31,29 +31,29 @@ public protocol IGRCropViewDelegate : class {
 }
 
 public class IGRCropView: UIView {
-    
+
     //MARK: - Public VARs
-    
+
     /*
      The optional View Delegate.
      */
-    
+
     weak var delegate: IGRCropViewDelegate?
-    
+
     //MARK: - Private VARs
-    
+
     internal lazy var horizontalCropLines: [IGRCropLine] = { [unowned self] by in
         var lines = self.setupHorisontalLines(count: self.cropLinesCount,
                                               className: IGRCropLine.self)
         return lines as! [IGRCropLine]
         }(())
-    
+
     internal lazy var verticalCropLines: [IGRCropLine] = { [unowned self] by in
         var lines = self.setupVerticalLines(count: self.cropLinesCount,
                                             className: IGRCropLine.self)
         return lines as! [IGRCropLine]
         }(())
-    
+
     internal lazy var horizontalGridLines: [IGRCropGridLine] = { [unowned self] by in
         var lines = self.setupHorisontalLines(count: self.gridLinesCount,
                                               className: IGRCropGridLine.self)
@@ -64,76 +64,117 @@ public class IGRCropView: UIView {
                                             className: IGRCropGridLine.self)
         return lines as! [IGRCropGridLine]
         }(())
-    
+
     internal var cornerBorderLength      = kCropViewCornerLength
     internal var cornerBorderWidth       = kCropViewCornerWidth
-    
+
     internal var cropLinesCount         = kCropLinesCount
     internal var gridLinesCount         = kGridLinesCount
-    
+
     internal var isCropLinesDismissed: Bool  = true
     internal var isGridLinesDismissed: Bool  = true
-    
+
     internal var isAspectRatioLocked: Bool = false
     internal var aspectRatioWidth: CGFloat = CGFloat.zero
     internal var aspectRatioHeight: CGFloat = CGFloat.zero
-    
+
+    internal var currentControlState: ControlState?
+    internal var currentTargetFrame: CGRect = .zero
+
     // MARK: - Life Cicle
-    
+
     init(frame: CGRect,
          cornerBorderWidth: CGFloat,
          cornerBorderLength: CGFloat,
          cropLinesCount: Int,
          gridLinesCount: Int) {
         super.init(frame: frame)
-        
+
         self.cornerBorderLength = cornerBorderLength
         self.cornerBorderWidth = cornerBorderWidth
-        
+
         self.cropLinesCount = cropLinesCount
         self.gridLinesCount = gridLinesCount
-        
+
         setup()
     }
-    
+
     required public init?(coder aDecoder: NSCoder) {
         super.init(coder: aDecoder)
-        
+
         setup()
     }
-    
+
     fileprivate func setup() {
-        
+
         self.setupLines()
-        
+
         let upperLeft = IGRCropCornerView(cornerType: .upperLeft,
                                           lineWidth: cornerBorderWidth,
                                           lineLenght: cornerBorderLength)
         upperLeft.center = CGPoint(x: cornerBorderLength.half,
                                    y: cornerBorderLength.half)
         self.addSubview(upperLeft)
-        
+
         let upperRight = IGRCropCornerView(cornerType: .upperRight,
                                            lineWidth: cornerBorderWidth,
                                            lineLenght:cornerBorderLength)
         upperRight.center = CGPoint(x: (self.frame.size.width - cornerBorderLength.half),
                                     y: cornerBorderLength.half)
         self.addSubview(upperRight)
-        
+
         let lowerRight = IGRCropCornerView(cornerType: .lowerRight,
                                            lineWidth: cornerBorderWidth,
                                            lineLenght:cornerBorderLength)
         lowerRight.center = CGPoint(x: (self.frame.size.width - cornerBorderLength.half),
                                     y: (self.frame.size.height - cornerBorderLength.half))
         self.addSubview(lowerRight)
-        
+
         let lowerLeft = IGRCropCornerView(cornerType: .lowerLeft,
                                           lineWidth: cornerBorderWidth,
                                           lineLenght:cornerBorderLength)
         lowerLeft.center = CGPoint(x: cornerBorderLength.half,
                                    y: (self.frame.size.height - cornerBorderLength.half))
         self.addSubview(lowerLeft)
-        
+
         resetAspectRect()
+    }
+}
+
+enum ControlState {
+    case topLeft
+    case topRight
+    case bottomLeft
+    case bottomRight
+    case top
+    case bottom
+    case left
+    case right
+
+    static func locate(with location: CGPoint, on target: CGRect) -> ControlState? {
+        let topLeft = CGPoint(x: CGFloat.zero, y: CGFloat.zero)
+        let topRight = CGPoint(x: target.size.width, y: CGFloat.zero)
+        let bottomLeft = CGPoint(x: CGFloat.zero, y: target.size.height)
+        let bottomRight = CGPoint(x: target.size.width, y: target.size.height)
+
+        if location.distanceTo(point: topLeft) < kCropViewHotArea {
+            return .topLeft
+        } else if location.distanceTo(point: topRight) < kCropViewHotArea {
+            return .topRight
+        } else if location.distanceTo(point: bottomLeft) < kCropViewHotArea {
+            return .bottomLeft
+        } else if location.distanceTo(point: bottomRight) < kCropViewHotArea {
+            return .bottomRight
+        } else if abs(location.x - topLeft.x) < kCropViewHotArea {
+            return .left
+        } else if abs(location.x - topRight.x) < kCropViewHotArea {
+            return .right
+        } else if abs(location.y - topLeft.y) < kCropViewHotArea {
+            return .top
+        } else if abs(location.y - bottomLeft.y) < kCropViewHotArea {
+            return .bottom
+        }
+
+        return nil
     }
 }
